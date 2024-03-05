c7019455e8d90ecc2b27d85853dc31b411096854
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-03-05 19:41:09 -0300

    finalizando os estilos do componente choices

--- src/components/HeaderCompoent.vue
+++ src/components/HeaderCompoent.vue
@@ -3,7 +3,7 @@
 <template>
     <nav class="sm:container sm:mx-auto">
         <div class="py-4 px-4 sm:px-0">
-            <h2 class="font-bold text-2xl text-slate-800">Assemble<span class="text-red-600 font-bold text-2xl">Snack</span></h2>
+            <h2 class="font-bold text-2xl text-slate-800">Assemble<span class="text-[#d7263d] font-bold text-2xl">Snack</span></h2>
         </div>
     </nav>
 </template>
