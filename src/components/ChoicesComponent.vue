608d3e1829c9e91f804b016df2af903fc6492666
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-03-07 17:35:23 -0300

    criando botao de fazer pedido

--- src/components/ChoicesComponent.vue
+++ src/components/ChoicesComponent.vue
@@ -1,72 +1,77 @@
 <template>
-    <div class="px-5 py-10 mt-14 mb-10 md:mt-0 bg-[#863e57] rounded-2xl md:p-12">
-        <div class="mb-6">
-            <h1 class="font-bold text-slate-100 text-xl md:text-3xl">Escolha seus ingredientes: </h1>
-        </div>
-        <div class="px-4 mt-9 md:mt-2 md:mb-9">
-            <h2 class="font-semibold text-xl text-slate-100">Pão</h2>
-            <div class="flex px-4 md:mt-0">
-                <div>
-                    <input type="radio" name="pao" id="gergelim" class="mr-1 accent-[#64b5f6]" value="gergelim"
-                        v-model="input_pao">
-                    <label class="text-slate-100" for="gergelim">Gergelim</label>
+    <div>
+        <div class="px-5 py-10 mt-14 mb-10 md:mt-0 bg-[#692d42] rounded-2xl md:p-12">
+            <div class="mb-6">
+                <h1 class="font-bold text-slate-100 text-xl md:text-3xl">Escolha seus ingredientes: </h1>
+            </div>
+            <div class="px-4 mt-9 md:mt-2 md:mb-9">
+                <h2 class="font-semibold text-xl text-slate-100">Pão</h2>
+                <div class="flex px-4 md:mt-0">
+                    <div>
+                        <input type="radio" name="pao" id="gergelim" class="mr-1 accent-[#64b5f6]" value="gergelim"
+                            v-model="input_pao">
+                        <label class="text-slate-100" for="gergelim">Gergelim</label>
+                    </div>
+                    <div class="mx-2">
+                        <input type="radio" name="pao" id="australiano" class="mr-1 accent-[#64b5f6]"
+                            value="australiano" v-model="input_pao">
+                        <label class="text-slate-100" for="australiano">Australiano</label>
+                    </div>
                 </div>
-                <div class="mx-2">
-                    <input type="radio" name="pao" id="australiano" class="mr-1 accent-[#64b5f6]" value="australiano"
-                        v-model="input_pao">
-                    <label class="text-slate-100" for="australiano">Australiano</label>
+            </div>
+            <div class="px-4 mt-4 md:mb-9">
+                <h2 class="font-semibold text-xl text-slate-100">Saladas</h2>
+                <div class="flex px-4 mt-2 md:mt-0">
+                    <div>
+                        <input type="checkbox" name="alface" id="alface" class="mr-1 accent-[#64b5f6]" value="alface"
+                            v-model="input_alface">
+                        <label class="text-slate-100" for="alface">Alface</label>
+                    </div>
+                </div>
+            </div>
+            <div class="px-4 mt-4 md:mb-9">
+                <h2 class="font-semibold text-xl text-slate-100">Molhos</h2>
+                <div class="flex px-4 mt-2 md:mt-0">
+                    <div>
+                        <input type="checkbox" name="ketchup" id="ketchup" class="mr-1 accent-[#64b5f6]" value="ketchup"
+                            v-model="input_molho">
+                        <label class="text-slate-100" for="ketchup">Ketchup</label>
+                    </div>
+                    <div class="mx-2">
+                        <input type="checkbox" name="mostarda" id="mostarda" value="mostarda"
+                            class="mr-1 accent-[#64b5f6]" v-model="input_molho">
+                        <label class="text-slate-100" for="mostarda">Mostarda</label>
+                    </div>
+                    <div class="mx-2">
+                        <input type="checkbox" name="maionese" id="maionese" value="maionese"
+                            class="mr-1 accent-[#64b5f6]" v-model="input_molho">
+                        <label class="text-slate-100" for="maionese">Maionese</label>
+                    </div>
+                </div>
+            </div>
+            <div class="px-4 mt-4">
+                <h2 class="font-semibold text-xl text-slate-100">Bife</h2>
+                <div class="flex px-4 mt-2 md:mt-0">
+                    <div>
+                        <input type="radio" name="hamburguer" id="bovino" value="bovino" v-model="input_hamburguer"
+                            class="mr-1 accent-[#64b5f6]">
+                        <label class="text-slate-100" for="bovino">Bovino</label>
+                    </div>
+                    <div class="mx-2">
+                        <input type="radio" name="hamburguer" id="frango" value="frango" v-model="input_hamburguer"
+                            class="mr-1 accent-[#64b5f6]">
+                        <label class="text-slate-100" for="frango">Frango</label>
+                    </div>
+                    <div class="mx-2">
+                        <input type="radio" name="hamburguer" id="soja" value="soja" v-model="input_hamburguer"
+                            class="mr-1 accent-[#64b5f6]">
+                        <label class="text-slate-100" for="soja">Soja</label>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="px-4 mt-4 md:mb-9">
-            <h2 class="font-semibold text-xl text-slate-100">Saladas</h2>
-            <div class="flex px-4 mt-2 md:mt-0">
-                <div>
-                    <input type="checkbox" name="alface" id="alface" class="mr-1 accent-[#64b5f6]" value="alface"
-                        v-model="input_alface">
-                    <label class="text-slate-100" for="alface">Alface</label>
-                </div>
-            </div>
-        </div>
-        <div class="px-4 mt-4 md:mb-9">
-            <h2 class="font-semibold text-xl text-slate-100">Molhos</h2>
-            <div class="flex px-4 mt-2 md:mt-0">
-                <div>
-                    <input type="checkbox" name="ketchup" id="ketchup" class="mr-1 accent-[#64b5f6]" value="ketchup"
-                        v-model="input_molho">
-                    <label class="text-slate-100" for="ketchup">Ketchup</label>
-                </div>
-                <div class="mx-2">
-                    <input type="checkbox" name="mostarda" id="mostarda" value="mostarda" class="mr-1 accent-[#64b5f6]"
-                        v-model="input_molho">
-                    <label class="text-slate-100" for="mostarda">Mostarda</label>
-                </div>
-                <div class="mx-2">
-                    <input type="checkbox" name="maionese" id="maionese" value="maionese" class="mr-1 accent-[#64b5f6]"
-                        v-model="input_molho">
-                    <label class="text-slate-100" for="maionese">Maionese</label>
-                </div>
-            </div>
-        </div>
-        <div class="px-4 mt-4">
-            <h2 class="font-semibold text-xl text-slate-100">Bife</h2>
-            <div class="flex px-4 mt-2 md:mt-0">
-                <div>
-                    <input type="radio" name="hamburguer" id="bovino" value="bovino" v-model="input_hamburguer"
-                        class="mr-1 accent-[#64b5f6]">
-                    <label class="text-slate-100" for="bovino">Bovino</label>
-                </div>
-                <div class="mx-2">
-                    <input type="radio" name="hamburguer" id="frango" value="frango" v-model="input_hamburguer"
-                        class="mr-1 accent-[#64b5f6]">
-                    <label class="text-slate-100" for="frango">Frango</label>
-                </div>
-                <div class="mx-2">
-                    <input type="radio" name="hamburguer" id="soja" value="soja" v-model="input_hamburguer"
-                        class="mr-1 accent-[#64b5f6]">
-                    <label class="text-slate-100" for="soja">Soja</label>
-                </div>
-            </div>
+        <div class="text-center md:text-right">
+            <button class="bg-[#275d8b] text-slate-100 px-8 py-2 rounded-md">Fazer pedido</button>
         </div>
     </div>
 </template>
